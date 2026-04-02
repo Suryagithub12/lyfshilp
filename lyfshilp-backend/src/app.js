3d2629d7fa30cfc775fb1d9f51fc6d01b5b22f03
Surya Gupta <surya.mca23.du@gmail.com>
2026-04-03 00:01:26 +0530

    add frontend url for deployment

--- lyfshilp-backend/src/app.js
+++ lyfshilp-backend/src/app.js
@@ -42,6 +42,7 @@ const allowedOrigins = [
   "http://localhost:5174", // ✅ Added for Vite alternate port
   "http://localhost:4000",
   "https://lyfshilp.vercel.app",
+  "https://lyfshilp-six.vercel.app/",
   "https://lyfshilp.com",
   "https://www.lyfshilp.com", // ✅ added live domain
   process.env.CLIENT_URL, // optional env variable
